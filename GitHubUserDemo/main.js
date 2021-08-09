fbaf4794cd080d6c973c16d24f6cbb1db349dffc
Charlie <charlie@deepbluec.com>
2021-08-09 15:29:59 +0100

    Improvement on code

--- GitHubUserDemo/main.js
+++ GitHubUserDemo/main.js
@@ -1,6 +1,6 @@
 async function getUserData() {
 
-    const repo = await fetch('https://api.github.com/users/charlie249/Agile_Metrics_Dashboard_Project');
+    const repo = await fetch('https://github.com/charlie249/Agile_Metrics_Dashboard_Project');
     const repoData = await repo.json();
     document.querySelector('#avatar').setAttribute('src', repoData.avatar_url);
     document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${repoData.labels}` // <strong> Followers: </strong> ${repoData.followers} <strong> Following: </strong> ${repoData.following} `
