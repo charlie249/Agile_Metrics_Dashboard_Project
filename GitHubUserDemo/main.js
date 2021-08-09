42452d4a6c13a586fa2ad0096ff7b35f96e00741
Charlie <charlie@deepbluec.com>
2021-08-09 14:42:31 +0100

    Started to edit the file to meet the requirements

--- GitHubUserDemo/main.js
+++ GitHubUserDemo/main.js
@@ -1,15 +1,11 @@
-
 async function getUserData() {
 
-    const user = await fetch('https://api.github.com/users/CMayo64');
+    const user = await fetch('https://api.github.com/users/charlie249/Agile_Metrics_Dashboard_Project');
     const userData = await user.json();
     document.querySelector('#avatar').setAttribute('src', userData.avatar_url);
-    document.querySelector('#info').innerHTML = `<strong> Bio: </strong> ${userData.bio} <strong> Followers: </strong> ${userData.followers} <strong> Following: </strong> ${userData.following} `
+    document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${userData.labels} <strong> Followers: </strong> ${userData.followers} <strong> Following: </strong> ${userData.following} `
 
 }
-
-
-
 
 window.addEventListener('DOMContentLoaded', () => {
     getUserData();
