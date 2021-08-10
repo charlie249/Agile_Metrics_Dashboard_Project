489372c4613a65a73d8cc64c23e2d5c81d8615ac
Charlie <charlie@deepbluec.com>
2021-08-10 10:53:30 +0100

    Tried to gather "pulls" information

--- GitHubUserDemo/main.js
+++ GitHubUserDemo/main.js
@@ -1,9 +1,11 @@
 async function getUserData() {
 
-    const user = await fetch('https://api.github.com/repos/serge-web/serge/issues');
+    const user = await fetch('https://api.github.com/repos/serge-web/serge');
     const userData = await user.json();
+    const pulls = await fetch('https://api.github.com/repos/serge-web/serge/pulls');
+    const pullsData = await pulls.json();
     //document.querySelector('#avatar').setAttribute('src', userData.avatar_url);
-    document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${userData} <strong> Proper: </strong> ${userData.followers}` //<strong> Following: </strong> ${userData.following} `
+    document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${userData.pullsData} <strong> Proper: </strong> ${userData.followers}` //<strong> Following: </strong> ${userData.following} `
     
    // var gs = require('github-scraper');
    // gs(user, function(err, data) {
