8541cf3c22792e5e584a45b91c6d1c208c6b2f23
Charlie <charlie@deepbluec.com>
2021-08-10 09:05:07 +0100

    Further code improvements

--- GitHubUserDemo/main.js
+++ GitHubUserDemo/main.js
@@ -1,9 +1,9 @@
 async function getUserData() {
 
-    const user = await fetch('https://api.github.com/users/CMayo64');
+    const user = await fetch('https://api.github.com/repos/Agile_Metrics_Dashboard_Project');
     const userData = await user.json();
-    document.querySelector('#avatar').setAttribute('src', userData.avatar_url);
-    document.querySelector('#info').innerHTML = `<strong> Bio: </strong> ${userData.bio} <strong> Followers: </strong> ${userData.followers} <strong> Following: </strong> ${userData.following} `
+    //document.querySelector('#avatar').setAttribute('src', userData.avatar_url);
+    document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${userData.Actions}` // <strong> Followers: </strong> ${userData.followers} <strong> Following: </strong> ${userData.following} `
 
 }
 
