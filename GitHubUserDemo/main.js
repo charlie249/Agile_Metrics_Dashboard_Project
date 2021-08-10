a6c06fb12db2d1c60b864f23d361203a27d8eed6
Charlie <charlie@deepbluec.com>
2021-08-10 10:21:55 +0100

    Testing out code

--- GitHubUserDemo/main.js
+++ GitHubUserDemo/main.js
@@ -1,10 +1,14 @@
 async function getUserData() {
 
-    const user = await fetch('https://api.github.com/repos/Agile_Metrics_Dashboard_Project');
+    const user = await fetch('https://api.github.com/repos/serge-web/serge');
     const userData = await user.json();
     //document.querySelector('#avatar').setAttribute('src', userData.avatar_url);
-    document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${userData.Actions}` // <strong> Followers: </strong> ${userData.followers} <strong> Following: </strong> ${userData.following} `
-
+    document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${userData} <strong> Proper: </strong> ${userData.followers}` //<strong> Following: </strong> ${userData.following} `
+    
+   // var gs = require('github-scraper');
+   // gs(user, function(err, data) {
+   //    document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${userData.data}`
+   // })
 }
 
 window.addEventListener('DOMContentLoaded', () => {
