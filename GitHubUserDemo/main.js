be6b0123fe9dfea6fc0e084b05bf3801041b7b00
Charlie <charlie@deepbluec.com>
2021-08-10 13:19:11 +0100

    Trying to gather "issues" information

--- GitHubUserDemo/main.js
+++ GitHubUserDemo/main.js
@@ -1,11 +1,9 @@
 async function getUserData() {
 
-    const repo = await fetch('https://api.github.com/repos/serge-web/serge');
+    const repo = await fetch('https://api.github.com/repos/serge-web/serge/issues');
     const repoData = await repo.json();
-    //const pullData = await pulls.json();
-    //const pull = await fetch('https://github.com/serge-web/serge/pull/987');
     //document.querySelector('#avatar').setAttribute('src', repoData.avatar_url);
-    document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${repoData} <strong> Proper: </strong> ${repoData.followers}` // <strong> Following: </strong> ${repoData.following} `
+    document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${repoData} <strong> Pull Requests: </strong> ${repoData.followers}` // <strong> Following: </strong> ${repoData.following} `
     
    // var gs = require('github-scraper');
    // gs(repo, function(err, data) {
