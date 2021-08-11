bfc98174bcf8bee73b02daf7ab3736091a6beaa0
Charlie <charlie@deepbluec.com>
2021-08-11 15:36:39 +0100

    Breakthrough

--- GitHubUserDemo/main.js
+++ GitHubUserDemo/main.js
@@ -3,19 +3,10 @@ async function getUserData() {
     const repo = await fetch('https://api.github.com/repos/serge-web/serge/issues/988');
     const repoData = await repo.json();
     //document.querySelector('#avatar').setAttribute('src', repoData.avatar_url);
-    document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${repoData.comments} <strong> Actions: </strong> ${repoData.actions}` // <strong> Following: </strong> ${repoData.following} `
-
-   //const userAction = async () => {
-      //  const response = await fetch('https://api.github.com/repos/serge-web/serge/issues');
-        //const myJson = await response.json(); //extract JSON from the http response
-        //document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${myJson} //<strong> Actions: </strong> ${repoData.actions}`
-     // }
-    // document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${repoData} <strong> Actions: </strong> ${repoData.actions}` // <strong> Following: </strong> ${repoData.following} `
-
-   // var gs = require('github-scraper');
-   // gs(repo, function(err, data) {
-   //    document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${userData.data}`
-   // })
+    document.querySelector('#title').innerHTML = `<strong> Title: </strong> ${repoData.title} <strong> Number: </strong> ${repoData.number} <strong> Comments: </strong> ${repoData.comments_url} `
+    document.querySelector('#number').innerHTML = `<strong> Number: </strong> ${repoData.number}`
+    document.querySelector('#comments').innerHTML = `<strong> Comments: </strong> ${repoData.comments_url} `
+    
 }
 
 window.addEventListener('DOMContentLoaded', () => {
