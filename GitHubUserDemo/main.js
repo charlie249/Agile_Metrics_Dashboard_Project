c88ce67256bfac94c1e6dd788d90a9c10e7166ab
Charlie <charlie@deepbluec.com>
2021-08-11 10:37:46 +0100

    Grabbing comments from specific issues

--- GitHubUserDemo/main.js
+++ GitHubUserDemo/main.js
@@ -1,9 +1,9 @@
 async function getUserData() {
 
-    const repo = await fetch('https://api.github.com/repos/serge-web/serge/issues');
+    const repo = await fetch('https://api.github.com/repos/serge-web/serge/issues/988');
     const repoData = await repo.json();
     //document.querySelector('#avatar').setAttribute('src', repoData.avatar_url);
-    document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${repoData} <strong> Actions: </strong> ${repoData.actions}` // <strong> Following: </strong> ${repoData.following} `
+    document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${repoData.comments} <strong> Actions: </strong> ${repoData.actions}` // <strong> Following: </strong> ${repoData.following} `
 
    //const userAction = async () => {
       //  const response = await fetch('https://api.github.com/repos/serge-web/serge/issues');
