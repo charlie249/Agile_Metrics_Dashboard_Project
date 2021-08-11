beb092e041e6ef6ca81a9dd1d2609ab1c8a636af
Charlie <charlie@deepbluec.com>
2021-08-11 09:56:17 +0100

    Code update

--- GitHubUserDemo/main.js
+++ GitHubUserDemo/main.js
@@ -3,8 +3,15 @@ async function getUserData() {
     const repo = await fetch('https://api.github.com/repos/serge-web/serge/issues');
     const repoData = await repo.json();
     //document.querySelector('#avatar').setAttribute('src', repoData.avatar_url);
-
     document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${repoData} <strong> Actions: </strong> ${repoData.actions}` // <strong> Following: </strong> ${repoData.following} `
+
+   //const userAction = async () => {
+      //  const response = await fetch('https://api.github.com/repos/serge-web/serge/issues');
+        //const myJson = await response.json(); //extract JSON from the http response
+        //document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${myJson} //<strong> Actions: </strong> ${repoData.actions}`
+     // }
+    // document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${repoData} <strong> Actions: </strong> ${repoData.actions}` // <strong> Following: </strong> ${repoData.following} `
+
    // var gs = require('github-scraper');
    // gs(repo, function(err, data) {
    //    document.querySelector('#info').innerHTML = `<strong> Labels: </strong> ${userData.data}`
