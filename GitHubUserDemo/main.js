1aeede1d3cfb8bea47782c311debbcce7e96aa60
Charlie <charlie@deepbluec.com>
2021-08-12 09:46:25 +0100

    Almost working code

--- GitHubUserDemo/main.js
+++ GitHubUserDemo/main.js
@@ -3,10 +3,9 @@ async function getUserData() {
     const repo = await fetch('https://api.github.com/repos/serge-web/serge/issues/988');
     const repoData = await repo.json();
     //document.querySelector('#avatar').setAttribute('src', repoData.avatar_url);
-    document.querySelector('#title').innerHTML = `<strong> Title: </strong> ${repoData.title} <strong> Number: </strong> ${repoData.number} <strong> Comments: </strong> ${repoData.comments_url} `
+    document.querySelector('#title').innerHTML = `<strong> Title: </strong> ${repoData.title}`
     document.querySelector('#number').innerHTML = `<strong> Number: </strong> ${repoData.number}`
-    document.querySelector('#comments').innerHTML = `<strong> Comments: </strong> ${repoData.comments_url} `
-    
+    document.querySelector('#comments').innerHTML = `<strong> Comments: </strong> ${repoData.comments_url} `   
 }
 
 window.addEventListener('DOMContentLoaded', () => {
