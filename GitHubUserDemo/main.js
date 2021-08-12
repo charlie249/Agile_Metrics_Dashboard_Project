7be0d7f623a74312d2e3f9f04005047e2a422b63
Charlie <charlie@deepbluec.com>
2021-08-12 09:46:25 +0100

    Almost working code

--- GitHubUserDemo/main.js
+++ GitHubUserDemo/main.js
@@ -2,11 +2,13 @@ async function getUserData() {
 
     const repo = await fetch('https://api.github.com/repos/serge-web/serge/issues/988');
     const repoData = await repo.json();
+    const rawComments = await fetch('https://api.github.com/repos/serge-web/serge/issues/988/comments');
+    const comments = await rawComments.json();
     //document.querySelector('#avatar').setAttribute('src', repoData.avatar_url);
-    document.querySelector('#title').innerHTML = `<strong> Title: </strong> ${repoData.title} <strong> Number: </strong> ${repoData.number} <strong> Comments: </strong> ${repoData.comments_url} `
+    document.querySelector('#id').innerHTML = `<strong> ID: </strong> ${repoData.id} ` 
+    document.querySelector('#title').innerHTML = `<strong> Issue Title: </strong> ${repoData.title}`
     document.querySelector('#number').innerHTML = `<strong> Number: </strong> ${repoData.number}`
-    document.querySelector('#comments').innerHTML = `<strong> Comments: </strong> ${repoData.comments_url} `
-    
+    document.querySelector('#comments').innerHTML = `<strong> Comments: </strong> ${comments} `   
 }
 
 window.addEventListener('DOMContentLoaded', () => {
