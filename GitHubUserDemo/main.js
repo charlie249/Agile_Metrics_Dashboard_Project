01f2d1de9359a912f3d6c22d4ff0e7a96d626744
Charlie <charlie@deepbluec.com>
2021-08-16 16:45:02 +0100

    Code update

--- GitHubUserDemo/main.js
+++ GitHubUserDemo/main.js
@@ -2,10 +2,10 @@ async function getUserData() {
 
     const repo = await fetch('https://api.github.com/repos/serge-web/serge/issues/988');
     const repoData = await repo.json();
-    //TODO make comments_url display actual data not just the url
-    var comment = String(comments);
-    var comments = await rawComments.text();
-    var rawComments = await fetch(repoData.comments_url);
+    //TODO!!!! make comments_url display actual data not just the url
+    //var comment = String(comments);
+    //var comments = await rawComments.text();
+    //var rawComments = await fetch(repoData.comments_url);
     //document.querySelector('#avatar').setAttribute('src', repoData.avatar_url);
     document.querySelector('#title').innerHTML = `<strong> Issue Title: </strong> ${repoData.title}`
     document.querySelector('#id').innerHTML = `<strong> ID: </strong> ${repoData.id} ` 
