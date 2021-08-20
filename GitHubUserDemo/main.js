31ee28414f2220e3de82e4c813fc3e17f1c3b048
Charlie <charlie@deepbluec.com>
2021-08-20 09:09:53 +0100

    Displayed everything I need to & started on calculation

--- GitHubUserDemo/main.js
+++ GitHubUserDemo/main.js
@@ -1,20 +1,20 @@
 async function getUserData() {
 
-    const repo = await fetch('https://api.github.com/repos/serge-web/serge/issues/988');
+    const repo = await fetch('https://api.github.com/repos/serge-web/serge/issues/994');
     const repoData = await repo.json();
-    const normal = await fetch('https://api.github.com/repos/serge-web/serge/issues/988');
-    const normalData = await normal.json();
-    //TODO!!!! make comments_url display actual data not just the url
-    // AND do the same for events_url
-    //const comments_url = await fetch('https://api.github.com/repos/serge-web/serge/issues/988/comments');
-    //const comments = comments_url.toString();
+    //TODO make comments_url and events_url display actual data not just the url
     //var rawComments = await fetch(repoData.comments_url);
+    //var comments = await rawComments.text();
+    //var comment = String(comments);
     //document.querySelector('#avatar').setAttribute('src', repoData.avatar_url);
     document.querySelector('#title').innerHTML = `<strong> Issue Title: </strong> ${repoData.title}`
     document.querySelector('#id').innerHTML = `<strong> ID: </strong> ${repoData.id} ` 
     document.querySelector('#number').innerHTML = `<strong> Number: </strong> ${repoData.number}`
-    document.querySelector('#comments').innerHTML = `<strong> Comments: </strong> ${repoData.comments} ` 
-    docuemnt.querySelector('#state').innerHTML = `<strong> State: </strong> ${normalData.state}`  
+    document.querySelector('#comments').innerHTML = `<strong> Comments: </strong> ${repoData.comments_url}`
+    document.querySelector('#state').innerHTML = `<strong> State: </strong> ${repoData.state}`   
+    document.querySelector('#created').innerHTML = `<strong> Created at: </strong> ${repoData.created_at}`
+    document.querySelector('#closed').innerHTML = `<strong> Closed at: </strong> ${repoData.closed_at}`
+
 }
 
 window.addEventListener('DOMContentLoaded', () => {
